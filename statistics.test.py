161a717bf845491c87a9bee3417f7a564695f77f
Kamal-TCQ <115541263+Kamal-TCQ@users.noreply.github.com>
2022-10-12 17:30:38 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -24,6 +24,8 @@ class StatsTest(unittest.TestCase):
     statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
     self.assertTrue(emailAlert.emailSent)
     self.assertTrue(ledAlert.ledGlows)
-
+  def EmailAlert(self):
+    return none
+  
 if __name__ == "__main__":
   unittest.main()
